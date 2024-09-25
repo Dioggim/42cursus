1e70da6b56ee7f4d875dfe90ccc6ddf445ec82c0
Diogo Almeida <diogo2991@gmail.com>
2024-09-25 16:49:28 +0200

    corrigindo incluindo atoi e strnstr

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: diogo <diogo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 17:11:54 by diogo             #+#    #+#             */
-/*   Updated: 2024/09/25 14:09:46 by diogo            ###   ########.fr       */
+/*   Updated: 2024/09/25 16:48:17 by diogo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,8 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-//char *ft_strnstr(const char *haystack, const char *needle, size_t len);
-//int ft_atoi(const char *str);
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
+int     ft_atoi(const char *str);
 
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
@@ -38,5 +38,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
+//void	*calloc(size_t num, size_t size);
 
 #endif
