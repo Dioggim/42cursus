1fb70c43750147d51708fcffbf7be60c3fc26779
Diogo Almeida <diogo2991@gmail.com>
2024-10-01 03:03:13 +0200

    Parte Obrigatoria finalizada

--- libft/libft.h
+++ libft/libft.h
@@ -6,13 +6,14 @@
 /*   By: diogo <diogo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 17:11:54 by diogo             #+#    #+#             */
-/*   Updated: 2024/09/30 19:42:41 by diogo            ###   ########.fr       */
+/*   Updated: 2024/10/01 02:59:14 by diogo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-# include <stdio.h>  // Inclui o cabeçalho necessário para 'size_t'
+# include <unistd.h>
+# include <stdio.h>
 # include <string.h>
 # include <stddef.h>
 # include <stdlib.h>
@@ -48,4 +49,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char *s1, char *s2);
 int		is_in_set(char c, const char *set);
 char	*ft_itoa(int n);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+
 #endif
