431b65bbe1aae897c9341099dde1b08a8e77c23c
Dioggim <diogo2991@gmail.com>
2024-12-05 16:36:48 +0100

    get_next_line

--- Get_next_line/get_next_line.c
+++ Get_next_line/get_next_line.c
@@ -6,18 +6,20 @@
 /*   By: dgimenez <dgimenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 16:47:16 by dgimenez          #+#    #+#             */
-/*   Updated: 2024/12/04 23:54:17 by dgimenez         ###   ########.fr       */
+/*   Updated: 2024/12/05 16:35:33 by dgimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *extract_line(char **buffer)
+char	*extract_line(char **buffer)
 {
-	char *newline_pos = gnl_strchr(*buffer, '\n');
-	char *line;
-	char *remaining;
+	char	*line;
+	char	*remaining;
+	char	*newline_pos;
+	size_t	len;
 
+	newline_pos = gnl_strchr(*buffer, '\n');
 	if (!newline_pos)
 	{
 		line = gnl_strdup(*buffer);
@@ -25,26 +27,24 @@ char *extract_line(char **buffer)
 		*buffer = NULL;
 		return (line);
 	}
-	size_t line_len = newline_pos - *buffer + 1;
-	line = malloc((line_len + 1) * sizeof(char));
+	len = newline_pos - *buffer + 1;
+	line = malloc((len + 1) * sizeof(char));
 	if (!line)
 		return (NULL);
-	for (size_t i = 0; i < line_len; i++)
-		line[i] = (*buffer)[i];
-	line[line_len] = '\0';
-
+	line[len] = '\0';
+	while (len--)
+		line[len] = (*buffer)[len];
 	remaining = gnl_strdup(newline_pos + 1);
 	free(*buffer);
 	*buffer = remaining;
-
 	return (line);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	static char *buffer;
-	char temp[BUFFER_SIZE + 1];
-	ssize_t bytes_read;
+	static char	*buffer;
+	char		temp[BUFFER_SIZE + 1];
+	ssize_t		bytes_read;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
@@ -52,7 +52,7 @@ char *get_next_line(int fd)
 	{
 		bytes_read = read(fd, temp, BUFFER_SIZE);
 		if (bytes_read <= 0)
-			break;
+			break ;
 		temp[bytes_read] = '\0';
 		buffer = gnl_strjoin(buffer, temp);
 	}
--- Get_next_line/get_next_line_utils.c
+++ Get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: dgimenez <dgimenez@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 23:33:38 by dgimenez          #+#    #+#             */
-/*   Updated: 2024/12/04 23:55:19 by dgimenez         ###   ########.fr       */
+/*   Updated: 2024/12/05 16:02:22 by dgimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 size_t	gnl_strlen(const char *s)
 {
-	size_t len = 0;
+	size_t	len;
+
+	len = 0;
 	while (s && s[len])
 		len++;
 	return (len);
@@ -35,27 +37,48 @@ char	*gnl_strchr(const char *s, int c)
 
 char	*gnl_strdup(const char *s)
 {
-	size_t len = gnl_strlen(s);
-	char *dup = malloc((len + 1) * sizeof(char));
+	size_t	len;
+	size_t	i;
+	char	*dup;
+
+	len = gnl_strlen(s);
+	dup = malloc((len + 1) * sizeof(char));
 	if (!dup)
 		return (NULL);
-	for (size_t i = 0; i < len; i++)
+	i = 0;
+	while (i < len)
+	{
 		dup[i] = s[i];
+		i++;
+	}
 	dup[len] = '\0';
 	return (dup);
 }
 
 char	*gnl_strjoin(char *s1, char *s2)
 {
-	size_t len1 = gnl_strlen(s1);
-	size_t len2 = gnl_strlen(s2);
-	char *joined = malloc((len1 + len2 + 1) * sizeof(char));
+	size_t	len1;
+	size_t	len2;
+	size_t	i;
+	char	*joined;
+
+	len1 = gnl_strlen(s1);
+	len2 = gnl_strlen(s2);
+	joined = malloc((len1 + len2 + 1) * sizeof(char));
 	if (!joined)
 		return (NULL);
-	for (size_t i = 0; i < len1; i++)
+	i = 0;
+	while (i < len1)
+	{
 		joined[i] = s1[i];
-	for (size_t i = 0; i < len2; i++)
+		i++;
+	}
+	i = 0;
+	while (i < len2)
+	{
 		joined[len1 + i] = s2[i];
+		i++;
+	}
 	joined[len1 + len2] = '\0';
 	free(s1);
 	return (joined);
